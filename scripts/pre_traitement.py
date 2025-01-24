72dbd5e13233bbe60ec983524a7b859e7742a2be
gabgab <orabonagabriel@gmail.com>
2025-01-24 14:40:50 +0000

    modif pre_traitements

--- scripts/pre_traitement.py
+++ scripts/pre_traitement.py
@@ -2,63 +2,37 @@ import geopandas as gpd
 from osgeo import gdal
 import os
 import numpy as np
-from my_function import supprimer_dossier_non_vide
+from my_function import (
+    pre_traitement_img,
+    supprimer_dossier_non_vide)
 import sys
 sys.path.append('/home/onyxia/work/libsigma')
 import read_and_write as rw
 
+racine = "/home/onyxia/work"
 
 ## Partie Serie_temp_S2_allbands.tif
 print("Partie pretraitement image")
 #  Définition des paramètres 
 
 print("Définitions des paramètres")
-input_raster_dir = "/home/onyxia/work/data/images"
-L_images = sorted(os.listdir(input_raster_dir))
-if ".keep" in L_images:
-    L_images.remove(".keep")
-shapefile_path = "/home/onyxia/work/data/project/emprise_etude.shp"
+input_raster_dir = os.path.join(racine,"data/images")
+l_images = sorted(os.listdir(input_raster_dir))
+if ".keep" in l_images:
+    l_images.remove(".keep")
+shapefile_path = os.path.join(racine,"data/project/emprise_etude.shp")
 
-output_dir = "/home/onyxia/work/output_pretraitement"
+output_dir = os.path.join(racine,"output_pretraitement")
 os.makedirs(output_dir, exist_ok=True)
 
-masque_path = "/home/onyxia/work/Depot_Git/results/data/img_pretraitees/masque_foret.tif"
+masque_path = os.path.join(racine,"Depot_Git/results/data/img_pretraitees/masque_foret.tif")
 
-# Charger le vecteur avec Geopandas
-emprise = gpd.read_file(shapefile_path).to_crs("EPSG:2154")
-
-# Extraire le GeoJSON sous forme de string
-print("Chargement du geojson en str")
-geojson_str = emprise.to_json()
-print("Chargement du geojson en str ok!")
-
-print("Traitements des images")
-for i,img in enumerate(L_images) :
-    date = img[11:19]
-    bande = img[53:]
-    ds_img = rw.open_image(os.path.join(input_raster_dir,img))
-    name_file = f"traitement_{date}_{bande}"
-    output_file = os.path.join(output_dir,name_file)
-    # Appliquer le clip avec GDAL
-    resolution = 10  # Résolution (10 m)
-    output_raster_test = gdal.Warp(
-        output_file, # Chemin de fichier, car on utilise GTiff
-        # "",  # Pas de chemin de fichier, car on utilise MEM
-        ds_img,  # Fichier en entrée (chemin ou objet gdal)
-        format = "GTiff", # Utiliser GTiff comme format
-        # format = "MEM",  # Utiliser MEM comme format
-        cutlineDSName = geojson_str,  # Passer directement le GeoJSON
-        cropToCutline = True,
-        outputType = gdal.GDT_UInt16, #UInt16
-        dstSRS = "EPSG:2154",  # Reprojection
-        xRes = resolution,  # Résolution X
-        yRes = resolution,  # Résolution Y
-        dstNodata = 0  # Valeur NoData
-    )
-    print(f"Image {i+1}/{len(L_images)} traitée")
-ds_img = None
-emprise = None
-geojson_str = None
+# Réalisation des pré-traitements sur les images individuelles 
+pre_traitement_img(
+    p_emprise = shapefile_path,
+    l_images = l_images,
+    input_raster_dir = input_raster_dir,
+    output_dir = output_dir)
 
 # Construction array
 print("Construction de l'array")
